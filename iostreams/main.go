b89bb692a62d36c9573994916228daae7cce3572
= <=>
2017-01-16 19:49:01 +0330

    parsing command line arguments

--- iostreams/main.go
+++ iostreams/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	log.Printf("%#v", os.Args)
 	//TODO parse args
-
+	log.Printf("%#v", os.Args)
+	sourceFilePath := os.Args[1]
+	destinationFilePath := os.Args[2]
+	fmt.Printf("%#v%#v", sourceFilePath, destinationFilePath)
 	//TODO open source file
 	//TODO open destination file
 	//TODO create variable for checksum
