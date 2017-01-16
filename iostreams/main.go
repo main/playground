54367681e288f8659f4672e3dd263350bbb8eb03
= <=>
2017-01-16 19:36:06 +0330

    planned

---
+++ iostreams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	log.Printf("%#v", os.Args)
+	//TODO parse args
+
+	//TODO open source file
+	//TODO open destination file
+	//TODO create variable for checksum
+	//TODO start copying file
+	//TODO start increment checksum variable
+
+}
